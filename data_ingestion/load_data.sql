e4f18c9f721326519439e693c98474499852f2b1
LucasBoden-da <lucashippertt2@gmail.com>
2025-01-30 08:35:08 -0300

    Update load_data.sql
    English Version

--- data_ingestion/load_data.sql
+++ data_ingestion/load_data.sql
@@ -2,59 +2,59 @@ CREATE SCHEMA IF NOT EXISTS techfab;
 
 CREATE TABLE IF NOT EXISTS techfab.dim_customer 
 (
-    sk_cliente integer NOT NULL,
-    id_cliente integer NOT NULL,
-    nome character varying(50) NOT NULL,
-    tipo character varying(50),
-    CONSTRAINT dim_customer_pkey PRIMARY KEY (sk_cliente)
+    sk_customer INTEGER NOT NULL,
+    id_customer INTEGER NOT NULL,
+    name VARCHAR(50) NOT NULL,
+    type VARCHAR(50),
+    CONSTRAINT dim_customer_pkey PRIMARY KEY (sk_customer)
 );
 
-CREATE TABLE IF NOT EXISTS techfab.dim_localidade
+CREATE TABLE IF NOT EXISTS techfab.dim_location
 (
-    sk_localidade integer NOT NULL,
-    id_localidade integer NOT NULL,
-    pais character varying(50) NOT NULL,
-    regiao character varying(50) NOT NULL,
-    estado character varying(50) NOT NULL,
-    cidade character varying(50) NOT NULL,
-    CONSTRAINT dim_localidade_pkey PRIMARY KEY (sk_localidade)
+    sk_location INTEGER NOT NULL,
+    id_location INTEGER NOT NULL,
+    country VARCHAR(50) NOT NULL,
+    region VARCHAR(50) NOT NULL,
+    state VARCHAR(50) NOT NULL,
+    city VARCHAR(50) NOT NULL,
+    CONSTRAINT dim_location_pkey PRIMARY KEY (sk_location)
 );
 
 CREATE TABLE IF NOT EXISTS techfab.dim_product
 (
-    sk_produto integer NOT NULL,
-    id_produto integer NOT NULL,
-    nome_produto character varying(50) NOT NULL,
-    categoria character varying(50) NOT NULL,
-    subcategoria character varying(50) NOT NULL,
-    CONSTRAINT dim_product_pkey PRIMARY KEY (sk_produto)
+    sk_product INTEGER NOT NULL,
+    id_product INTEGER NOT NULL,
+    product_name VARCHAR(50) NOT NULL,
+    category VARCHAR(50) NOT NULL,
+    subcategory VARCHAR(50) NOT NULL,
+    CONSTRAINT dim_product_pkey PRIMARY KEY (sk_product)
 );
 
 CREATE TABLE IF NOT EXISTS techfab.dim_time
 (
-    sk_tempo integer NOT NULL,
-    data_completa date,
-    ano integer NOT NULL,
-    mes integer NOT NULL,
-    dia integer NOT NULL,
-    CONSTRAINT dim_time_pkey PRIMARY KEY (sk_tempo)
+    sk_time INTEGER NOT NULL,
+    full_date DATE,
+    year INTEGER NOT NULL,
+    month INTEGER NOT NULL,
+    day INTEGER NOT NULL,
+    CONSTRAINT dim_time_pkey PRIMARY KEY (sk_time)
 );
 
 CREATE TABLE IF NOT EXISTS techfab.fact_sales
 (
-    sk_produto integer NOT NULL,
-    sk_cliente integer NOT NULL,
-    sk_localidade integer NOT NULL,
-    sk_tempo integer NOT NULL,
-    quantidade integer NOT NULL,
-    preco_venda numeric(10,2) NOT NULL,
-    custo_produto numeric(10,2) NOT NULL,
-    receita_vendas numeric(10,2) NOT NULL,
-    CONSTRAINT fact_sales_pkey PRIMARY KEY (sk_produto, sk_cliente, sk_localidade, sk_tempo),
-    CONSTRAINT fact_sales_sk_cliente_fkey FOREIGN KEY (sk_cliente) REFERENCES techfab.dim_customer (sk_cliente),
-    CONSTRAINT fact_sales_sk_localidade_fkey FOREIGN KEY (sk_localidade) REFERENCES techfab.dim_localidade (sk_localidade),
-    CONSTRAINT fact_sales_sk_produto_fkey FOREIGN KEY (sk_produto) REFERENCES techfab.dim_product (sk_produto),
-    CONSTRAINT fact_sales_sk_tempo_fkey FOREIGN KEY (sk_tempo) REFERENCES techfab.dim_time (sk_tempo)
+    sk_product INTEGER NOT NULL,
+    sk_customer INTEGER NOT NULL,
+    sk_location INTEGER NOT NULL,
+    sk_time INTEGER NOT NULL,
+    quantity INTEGER NOT NULL,
+    sale_price NUMERIC(10,2) NOT NULL,
+    product_cost NUMERIC(10,2) NOT NULL,
+    sales_revenue NUMERIC(10,2) NOT NULL,
+    CONSTRAINT fact_sales_pkey PRIMARY KEY (sk_product, sk_customer, sk_location, sk_time),
+    CONSTRAINT fact_sales_sk_customer_fkey FOREIGN KEY (sk_customer) REFERENCES techfab.dim_customer (sk_customer),
+    CONSTRAINT fact_sales_sk_location_fkey FOREIGN KEY (sk_location) REFERENCES techfab.dim_location (sk_location),
+    CONSTRAINT fact_sales_sk_product_fkey FOREIGN KEY (sk_product) REFERENCES techfab.dim_product (sk_product),
+    CONSTRAINT fact_sales_sk_time_fkey FOREIGN KEY (sk_time) REFERENCES techfab.dim_time (sk_time)
 );
 
 COPY techfab.dim_customer
@@ -78,6 +78,6 @@ IAM_ROLE 'arn:aws:iam::<YOUR_AWS_ACCOUNT_ID>:role/<YOUR_ROLE_NAME>'
 CSV;
 
 COPY techfab.fact_sales
-FROM '<PATH_FACT_SALES_FILE>'
+FROM 's3://<PATH_FACT_SALES_FILE>'
 IAM_ROLE 'arn:aws:iam::<YOUR_AWS_ACCOUNT_ID>:role/<YOUR_ROLE_NAME>'
 CSV;
