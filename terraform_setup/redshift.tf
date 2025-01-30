65095bc0226d0382fb5d4c303a73d8aaddc637bf
LucasBoden-da <lucashippertt2@gmail.com>
2025-01-30 07:58:15 -0300

    English translation

--- terraform_setup/redshift.tf
+++ terraform_setup/redshift.tf
@@ -1,4 +1,4 @@
-# Configura a Redshift VPC
+# Configure the Redshift VPC
 resource "aws_vpc" "redshift_vpc" {
   cidr_block = "10.0.0.0/16"
 
@@ -7,7 +7,7 @@ resource "aws_vpc" "redshift_vpc" {
   }
 }
 
-# Configura a Redshift Subnet
+# Configure the Redshift Subnet
 resource "aws_subnet" "redshift_subnet" {
   cidr_block = "10.0.1.0/24"
   vpc_id     = aws_vpc.redshift_vpc.id
@@ -17,7 +17,7 @@ resource "aws_subnet" "redshift_subnet" {
   }
 }
 
-# Configura um Gateway da Internet e Anexa a VPC
+# Configure an Internet Gateway and Attach it to the VPC
 resource "aws_internet_gateway" "redshift_igw" {
   vpc_id = aws_vpc.redshift_vpc.id
 
@@ -26,7 +26,7 @@ resource "aws_internet_gateway" "redshift_igw" {
   }
 }
 
-# Configura Uma Tabela de Roteamento
+# Configure a Route Table
 resource "aws_route_table" "redshift_route_table" {
   vpc_id = aws_vpc.redshift_vpc.id
 
@@ -40,13 +40,13 @@ resource "aws_route_table" "redshift_route_table" {
   }
 }
 
-# Associa a Tabela de Roteamento à Subnet
+# Associate the Route Table with the Subnet
 resource "aws_route_table_association" "redshift_route_table_association" {
   subnet_id      = aws_subnet.redshift_subnet.id
   route_table_id = aws_route_table.redshift_route_table.id
 }
 
-# Configura Um Grupo de Segurança de Acesso ao Data Warehouse com Redshift
+# Configure a Security Group for Redshift Data Warehouse Access
 resource "aws_security_group" "redshift_sg" {
   name        = "redshift_sg"
   description = "Allow Redshift traffic"
@@ -64,7 +64,7 @@ resource "aws_security_group" "redshift_sg" {
   }
 }
 
-# Configura Um Grupo de Subnets Redshift
+# Configure a Redshift Subnet Group
 resource "aws_redshift_subnet_group" "redshift_subnet_group" {
   name       = "redshift-subnet-group"
   subnet_ids = [aws_subnet.redshift_subnet.id]
@@ -74,7 +74,7 @@ resource "aws_redshift_subnet_group" "redshift_subnet_group" {
   }
 }
 
-# Configura Um Cluster Redshift 
+# Configure a Redshift Cluster
 resource "aws_redshift_cluster" "redshift_cluster" {
   cluster_identifier = var.redshift_cluster_id
   database_name      = "techfab"
